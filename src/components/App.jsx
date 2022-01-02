8b5b57f2126b57e8431e2670e1bf65247d50ae44
Kaung Khant Zaw <kaungkzdev@gmail.com>
2022-01-02 16:14:43 +0630

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="wrapper w-full h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-2xl font-bold underline test-title">Hello world!</h1>
+    </div>
+  );
+}
